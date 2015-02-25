aaa6ca969dddd35e36c4dc9eb2de89f82105f888
hastebrot <hastebrot@gmail.com>
2015-02-25 17:39:59 +0100

    (feat): Add UrlBuilder.

---
+++ relnotes-plugin/src/main/groovy/com/github/entera/gradle/relnotes/provider/github/UrlBuilder.groovy
@@ -0,0 +1,43 @@
+package com.github.entera.gradle.relnotes.provider.github
+
+class UrlBuilder {
+    String repo
+    String resource
+    String params
+    Integer page
+
+    private UrlBuilder() {}
+
+    static UrlBuilder build() {
+        return new UrlBuilder()
+    }
+
+    UrlBuilder repo(String repo) {
+        this.repo = repo; this
+    }
+
+    UrlBuilder resource(String resource) {
+        this.resource = resource; this
+    }
+
+    UrlBuilder params(String params) {
+        this.params = params; this
+    }
+
+    UrlBuilder page(Integer page) {
+        this.page = page; this
+    }
+
+    URL toUrl() {
+        def urlString = "https://api.github.com/"
+        urlString += "repos/${repo}/"
+        urlString += "${resource}?"
+        if (page != null) {
+            urlString += "page=${page}&"
+        }
+        if (params != null) {
+            urlString += "${params}&"
+        }
+        return urlString.minus(~/(&+)$/).toURL()
+    }
+}
---
+++ relnotes-plugin/src/test/groovy/com/github/entera/gradle/relnotes/provider/github/UrlBuilderTest.groovy
@@ -0,0 +1,36 @@
+package com.github.entera.gradle.relnotes.provider.github
+
+import org.hamcrest.Matchers
+import org.junit.Test
+
+import static org.junit.Assert.assertThat
+
+class UrlBuilderTest {
+    @Test
+    void "#toUrl()"() {
+        // given:
+        def urlBuilder = UrlBuilder.build()
+
+        // expect:
+        urlBuilder.repo(":ownerRepo").resource(":entityType").params(":params")
+        assertThat(urlBuilder.toUrl(), Matchers.is(
+            new URL("https://api.github.com/repos/:ownerRepo/:entityType?:params")
+        ))
+    }
+
+    @Test
+    void "#page()"() {
+        // given:
+        def urlBuilder = UrlBuilder.build().repo(":ownerRepo").resource(":entityType")
+
+        // expect:
+        assertThat(urlBuilder.page(36).toUrl(), Matchers.is(
+            new URL("https://api.github.com/repos/:ownerRepo/:entityType?page=36")
+        ))
+
+        // expect:
+        assertThat(urlBuilder.page(37).toUrl(), Matchers.is(
+            new URL("https://api.github.com/repos/:ownerRepo/:entityType?page=37")
+        ))
+    }
+}

e7bd83fbbedaa7037108e033df723d8d347dd03d
hastebrot <hastebrot@gmail.com>
2015-02-25 23:39:39 +0100

    (feat) Add RequestController.

---
+++ relnotes-plugin/src/main/groovy/com/github/entera/gradle/relnotes/provider/github/RequestController.groovy
@@ -0,0 +1,36 @@
+package com.github.entera.gradle.relnotes.provider.github
+
+import java.util.function.Consumer
+import java.util.function.Predicate
+
+class RequestController<C> {
+    final C context
+    Predicate<C> contextPredicate
+
+    public RequestController(C context) {
+        this.context = context
+    }
+
+    void updateContext(Consumer<C> contextConsumer) {
+        contextConsumer.accept(context)
+    }
+
+    boolean checkPredicate() {
+        if (!isPredicateNull(this.context, this.contextPredicate)) {
+            if (isPredicateTrue(this.context, this.contextPredicate)) {
+                return true
+            }
+        }
+        return false
+    }
+
+    private boolean isPredicateNull(Object value,
+                                    Predicate predicate) {
+        return value == null || predicate == null
+    }
+
+    private boolean isPredicateTrue(Object value,
+                                    Predicate predicate) {
+        return predicate.test(value)
+    }
+}
